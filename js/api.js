a6115415152c784b8191e23da0cfa66dd4246c5e
parkkkkjuneHyeon <gktkwk91@naver.com>
2025-05-29 13:16:44 +0900

    api 수정

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://g575dfbc1dbf538-dev.adb.ap-seoul-1.oraclecloudapps.com/ords/ad/v1/ad/';
-
+// const BASE_URL = 'https://g575dfbc1dbf538-dev.adb.ap-seoul-1.oraclecloudapps.com/ords/ad/v1/ad/';
+const BASE_URL = 'https://g4c02c303daba23-phiworks1.adb.ap-chuncheon-1.oraclecloudapps.com/ords/r/ad/';
 const DEVICE_URL = 'devices';
 const POSITION_URL = 'devices/position';
 const POSITION_LOCKED_URL = 'devices/position/locked';
