5d2987037836b7dc625534b9eec7b6b754058f4c
parkkkkjuneHyeon <gktkwk91@naver.com>
2025-06-02 13:52:35 +0900

    웹소켓수정

--- js/ws.js
+++ js/ws.js
@@ -59,7 +59,7 @@ const initWebsocket = () => {
     timeout: 3,
     onSuccess: onConnect,
     onFailure: onFailure,
-    userName: 'ADMIN',
+    userName: 'spacebank',
     password: 'demo00',
     reconnect: true,
     // reconnectInterval: 10,
